1d0c0ec4091a175be8da3ad43ae7bb083116895e
lol <gge>
2023-03-25 17:29:27 +0100

    rework

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <shader.h>
 #include <commons.h>
@@ -63,13 +62,81 @@ int main()
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init("#version 130");
         ImGui::StyleColorsDark();
-
+        //create varibles
+        int num_layers = 3;
+        int activation_function = 0;
+        int* topology;
+        topology = (int*)malloc(sizeof(int)*num_layers);
+        for (size_t i = 0; i < num_layers; i++)
+        {
+            topology[i] = 3;
+        }
+        
         while(!glfwWindowShouldClose(window)){
             glClear(GL_COLOR_BUFFER_BIT);
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFram    }
+        
+            ImGui::NewFrame();
 
+
+            ImGui::Begin("Multilayer Perceptron");
+            //count and display the number of parameters
+            int num_params = 0;
+            for (size_t i = 0; i < num_layers; i++)
+            {
+                num_params += topology[i];
+            }
+            ImGui::Text("Number of Parameters: %d", num_params);
+            ImGui::Separator();
+
+            if(ImGui::InputInt("Number of Layers", &num_layers, 1)){
+                if(num_layers < 2){
+                    num_layers = 2;
+                }
+                //allocate a clear piece of memory for the new topology
+                int* _topology = (int*)malloc(sizeof(int)*num_layers);
+                memset(_topology, 0, sizeof(int)*num_layers);
+
+                memcpy(_topology, topology, sizeof(int)*num_layers);
+                free(topology);
+                topology = _topology;
+            }
+            ImGui::Text("Activation Function");
+            ImGui::RadioButton("Sigmoid", &activation_function, 0);
+            ImGui::RadioButton("Tanh", &activation_function, 1);
+            ImGui::RadioButton("ReLU", &activation_function, 2);
+            ImGui::RadioButton("Leaky ReLU", &activation_function, 3);
+            ImGui::RadioButton("Softmax", &activation_function, 4);
+            ImGui::Separator();
+
+            //custom element for setting up the number of neurons in each layer
+            for (int i = 1; i <= num_layers; i++)
+            {
+                // input layer
+                if (i == 1)
+                {
+                    ImGui::InputInt("Input Layer", &topology[i-1], 1);
+                    ImGui::Separator();
+                    continue;
+                }
+                //output layer
+                else if (i == num_layers)
+                {
+                    ImGui::InputInt("Output Layer", &topology[i-1], 1);
+                    ImGui::Separator();
+                    continue;
+                }
+
+                // hidden layers
+                std::string LayerIDFormattedToText = "Layer " + std::to_string(i);
+                ImGui::InputInt(LayerIDFormattedToText.c_str(), &topology[i-1], 1);
+                ImGui::Separator();
+
+            }
+            
+
+            ImGui::End();
          
             ImGui::Render();
             
